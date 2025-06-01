81409b26a46f891b9df980bb33054948641d1d71
rgeniza <reidgeniza@usf.edu>
2025-06-01 11:04:06 -0700

    Add files via upload

--- budget_assistant.js
+++ budget_assistant.js
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+let startupName = "Quant";
+let initialCap = 1000000;
+let monthlyRev = 250000;
+let monthlyExp = 200000;
+let netCashFlow = monthlyRev-monthlyExp;
+let currentBalance = initialCap + netCashFlow;
+let isProfitable = netCashFlow > 0;
+console.log(startupName)
+console.log(netCashFlow)
+console.log(currentBalance)
+console.log(isProfitable)
+=======
 let startupName = "Quant";
 let initialCap = 1000000;
 let monthlyRev = 250000;
@@ -6,3 +19,4 @@ let netCashFlow = monthlyRev-monthlyExp;
 let currentBalance = initialCap + netCashFlow;
 let isProfitable = netCashFlow > 0;
 console.log()
+>>>>>>> 24f25aa4e900b3155e92ccde7615be30362cb6d2
