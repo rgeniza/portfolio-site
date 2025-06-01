911f130057cbe0e45c3e0bd179c69e510094f71d
rgeniza <reidgeniza@usf.edu>
2025-06-01 11:07:59 -0700

    Budget Assistant

--- budget_assistant.js
+++ budget_assistant.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let startupName = "Quant";
 let initialCap = 1000000;
 let monthlyRev = 250000;
@@ -10,13 +9,3 @@ console.log(startupName)
 console.log(netCashFlow)
 console.log(currentBalance)
 console.log(isProfitable)
-=======
-let startupName = "Quant";
-let initialCap = 1000000;
-let monthlyRev = 250000;
-let monthlyExp = 200000;
-let netCashFlow = monthlyRev-monthlyExp;
-let currentBalance = initialCap + netCashFlow;
-let isProfitable = netCashFlow > 0;
-console.log()
->>>>>>> 24f25aa4e900b3155e92ccde7615be30362cb6d2
