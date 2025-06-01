24f25aa4e900b3155e92ccde7615be30362cb6d2
rgeniza <reidgeniza@usf.edu>
2025-06-01 10:55:17 -0700

    Add files via upload

---
+++ budget_assistant.js
@@ -0,0 +1,8 @@
+let startupName = "Quant";
+let initialCap = 1000000;
+let monthlyRev = 250000;
+let monthlyExp = 200000;
+let netCashFlow = monthlyRev-monthlyExp;
+let currentBalance = initialCap + netCashFlow;
+let isProfitable = netCashFlow > 0;
+console.log()
